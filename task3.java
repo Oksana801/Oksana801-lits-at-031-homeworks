317e98ca2ae2fdc52f7b58227cda030b272e37f4
Oksana801 <49590974+Oksana801@users.noreply.github.com>
2020-11-30 22:24:35 +0200

    task3.changes

--- task3.java
+++ task3.java
@@ -9,24 +9,24 @@ public class task3 {
 
 
     public static void main(String[] args) {
-        Integer[] masyv = { 3, 43,7,7,0, 54, 3,1, 2,0, 6, 43,0,1,0, 2,};
-        Integer min = masyv[0];
-        Integer average = 0;
-        for (int i = masyv.length - 1; i >= 0; i--) {
-            if (min > masyv[i])
-                min = masyv[i];
+        Integer [] array = {3,43,7,7,0,54,3,1,2,0,6,43,0,1,0,2};
+        int min = 0;
+        int average;
+        for (int i = array.length - 1; i >= 0; i--) {
+            if (min > array[i])
+                min = array[i];
 
         }
         System.out.println("min " +  min);
 
-        if (masyv.length >0){
+        if (array.length >0){
             int sum =0;
-            for (int j=0; j< masyv.length; j++) {
-                sum +=masyv[j];
+            for (int j=0; j< array.length; j++) {
+                sum +=array[j];
             }
-            average =sum/masyv.length;
+            average =sum/array.length;
             System.out.println("average " + average);
-            List<Integer> list = Arrays.asList(masyv);
+            List<Integer> list = Arrays.asList(array);
             list.set(list.lastIndexOf(min), average);
             System.out.println("update last min element" +list);
 
