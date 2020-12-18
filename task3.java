ddeed3df7bb22c65f2fd5a18a0291cda3ee678d2
Oksana801 <49590974+Oksana801@users.noreply.github.com>
2020-12-18 16:28:01 +0200

    homework3.task3

--- task3.java
+++ task3.java
@@ -1,36 +1,39 @@
-package com.Selenium.homework1;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class task3 {
+    public static void main(String[] args) throws IOException {
 
+        File file = new File("src/test/java/file");
+        Map<String, String> allFileMap = new HashMap<>();
 
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
 
-    public static void main(String[] args) {
-        Integer [] array = {3,43,7,7,0,54,3,1,2,0,6,43,0,1,0,2};
-        int min = 0;
-        int average;
-        for (int i = array.length - 1; i >= 0; i--) {
-            if (min > array[i])
-                min = array[i];
+        String line;
+        int key = 1;
 
+        while ((line = br.readLine()) != null) {
+            allFileMap.put(String.valueOf(key), line);
+            key++;
+            System.out.println(line);
         }
-        System.out.println("min " +  min);
+        br.close();
+        fr.close();
 
-        if (array.length >0){
-            int sum =0;
-            for (int j=0; j< array.length; j++) {
-                sum +=array[j];
+        File file1 = new File("src/test/java/powerOfTwo.txt");
+
+        PrintWriter writer = new PrintWriter(file1);
+        for (String tempKey : allFileMap.keySet()) {
+            if (isPowerOfTwo(Integer.valueOf(tempKey))) {
+                writer.println(allFileMap.get(tempKey));
             }
-            average =sum/array.length;
-            System.out.println("average " + average);
-            List<Integer> list = Arrays.asList(array);
-            list.set(list.lastIndexOf(min), average);
-            System.out.println("update last min element" +list);
-
         }
+        writer.close();
     }
 
+    private static boolean isPowerOfTwo(long x) {
+        return x > 0 && (x & (x - 1)) == 0;
+    }
 }
