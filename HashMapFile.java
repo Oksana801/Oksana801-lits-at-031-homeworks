766320d72faa466d00723e715a5f37e7578faa58
Oksana801 <49590974+Oksana801@users.noreply.github.com>
2021-01-18 18:32:44 +0200

    changes to task3

---
+++ HashMapFile.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+
+    public class HashMapFile {
+        public static void main(String[] args) throws IOException {
+
+            File file = new File("src/test/java/file");
+            Map<String, String> allFileMap = new HashMap<>();
+
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+
+            String line;
+            int key = 1;
+
+            while ((line = br.readLine()) != null) {
+                allFileMap.put(String.valueOf(key), line);
+                key++;
+                System.out.println(line);
+            }
+            br.close();
+            fr.close();
+
+            File file1 = new File("src/test/java/powerOfTwo.txt");
+
+            PrintWriter writer = new PrintWriter(file1);
+            for (String tempKey : allFileMap.keySet()) {
+                if (isPowerOfTwo(Integer.valueOf(tempKey))) {
+                    writer.println(allFileMap.get(tempKey));
+                }
+            }
+            writer.close();
+        }
+
+        private static boolean isPowerOfTwo(long x) {
+            return x > 0 && (x & (x - 1)) == 0;
+        }
+    }
+
+
