59e51e60c2ed8eb31114affec91fe05a131ff274
Oksana801 <49590974+Oksana801@users.noreply.github.com>
2021-01-18 19:04:57 +0200

    changes to task2

---
+++ DateLocal.java
@@ -0,0 +1,23 @@
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+public class DateLocal {
+
+    static LocalDate myMethod(LocalDate date1, int days, int month, int year) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-mm-dd");
+        LocalDate futureDate = LocalDate.now().plusDays(days).plusMonths(month).plusYears(year);
+        return LocalDate.now().plusDays(days).plusMonths(month).plusYears(year);
+    }
+
+public class Main {
+
+}
+
+    public static void main(String[] args) {
+        DateLocal t2 =new DateLocal();
+
+        System.out.println ("active date " + LocalDate.now());
+        System.out.println("future date " + t2.myMethod( LocalDate.now(),6,2,10));
+
+    }
+}
