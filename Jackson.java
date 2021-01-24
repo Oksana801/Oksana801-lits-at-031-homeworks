6dcf46f5ac2aef62226a4ddf3f93463af4457895
Oksana801 <49590974+Oksana801@users.noreply.github.com>
2021-01-24 12:50:46 +0200

    Jackson.changes

--- Jackson.java
+++ Jackson.java
@@ -1,58 +1,28 @@
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Jackson {
     public static void main(String[] st) throws IOException, FileNotFoundException {
         ObjectMapper mapper = new ObjectMapper();
-        String json ="{\n" +
-                "  \"work\": {\n" +
-                "\n" +
-                "    \"members\": [\n" +
-                "      {\n" +
-                "        \"name\": \"Molecule Man\",\n" +
-                "        \"age\": 29,\n" +
-                "        \"secretIdentity\": \"Dan Jukes\",\n" +
-                "        \"powers\": [\n" +
-                "          \"Radiation resistance\",\n" +
-                "          \"Turning tiny\",\n" +
-                "          \"Radiation blast\"\n" +
-                "        ]\n" +
-                "      },\n" +
-                "      {\n" +
-                "        \"name\": \"Madame Uppercut\",\n" +
-                "        \"age\": 39,\n" +
-                "        \"secretIdentity\": \"Jane Wilson\",\n" +
-                "        \"powers\": [\n" +
-                "          \"Million tonne punch\",\n" +
-                "          \"Damage resistance\",\n" +
-                "          \"Superhuman reflexes\"\n" +
-                "        ]\n" +
-                "      }\n" +
-                "\n" +
-                "    ]\n" +
-                "  }\n" +
-                "}";
+        File from = new File("src/main/resources/Sample.json");
+        TypeReference<HashMap<String, Object>> typeRef
+                = new TypeReference<HashMap<String, Object>>() {
+        };
 
-        try {
-
-            // convert JSON string to Map
-            Map<String, String> map = mapper.readValue(json, Map.class);
-
-
-
-            System.out.println(map);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        HashMap<String, Object> o = mapper.readValue(from, typeRef);
+        System.out.println(o);
 
     }
+
 }
+
 
 
 
