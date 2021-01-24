2c2bb34ee82330b0c02d084d15158b3a5cd96786
Oksana801 <49590974+Oksana801@users.noreply.github.com>
2021-01-24 13:08:41 +0200

    changes

--- DateLocal.java
+++ DateLocal.java
@@ -3,10 +3,10 @@ import java.time.format.DateTimeFormatter;
 
 public class DateLocal {
 
-    static LocalDate myMethod(LocalDate date1, int days, int month, int year) {
+    static LocalDate myMethod(LocalDate currentDate, int days, int month, int year) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-mm-dd");
         LocalDate futureDate = LocalDate.now().plusDays(days).plusMonths(month).plusYears(year);
-        return LocalDate.now().plusDays(days).plusMonths(month).plusYears(year);
+        return futureDate;
     }
 
 public class Main {
