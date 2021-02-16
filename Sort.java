24b00454daebe1a18e671f6920c3d96d432ac704
Oksana801 <49590974+Oksana801@users.noreply.github.com>
2021-02-16 21:23:16 +0200

    Sort, changes

--- Sort.java
+++ Sort.java
@@ -1,32 +1,43 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.util.*;
 
 public class Sort {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Employee worker = new Employee();
         File file = new File("child/src/test/java/Employees.txt");
-        FileReader fr = new FileReader(file);
-        BufferedReader br = new BufferedReader(fr);
-
-        String line;
-        Employee tempEmployee;
-        List<Employee> workers = new ArrayList<Employee>();
-        while ((line = br.readLine()) != null) {
-            String[] splited = line.split("\\s+");
-            tempEmployee = new Employee(Integer.parseInt(splited[0]), splited[1], splited[2], Integer.parseInt(splited[3]));
-            workers.add(tempEmployee);
+        FileReader fr = null;
+        try {
+            fr = new FileReader(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
-        br.close();
-        fr.close();
+        try {
+            BufferedReader br = new BufferedReader(fr);
 
-        Collections.sort(workers);
-        for (Employee empl : workers) {
-            System.out.println(empl);
+            String line;
+            Employee tempEmployee;
+            List<Employee> workers = new ArrayList<Employee>();
+            while ((line = br.readLine()) != null) {
+                String[] splited = line.split("\\s+");
+                tempEmployee = new Employee(Integer.parseInt(splited[0]), splited[1], splited[2], Integer.parseInt(splited[3]));
+                workers.add(tempEmployee);
+            }
+            br.close();
+            fr.close();
+            Collections.sort(workers);
+            for (Employee empl : workers) {
+                System.out.println(empl);
 
-
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
