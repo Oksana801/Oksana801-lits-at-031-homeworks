cac633a50a1b39ab3e625e98f8cc46741d5cdd29
Oksana801 <49590974+Oksana801@users.noreply.github.com>
2021-02-16 21:10:38 +0200

    Employee, changes

--- Employee.java
+++ Employee.java
@@ -1,61 +1,65 @@
-public class Employee {
-    private int Id;
-    private String Name;
-    private String Position;
+public class Employee implements Comparable<Employee> {
+    private int id;
+    private String name;
+    private String position;
+    private int salary;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int ID) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        position = position;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        salary = salary;
+    }
+
+    public Employee(int id, String name, String position, int salary) {
+        this.id = id;
+        this.name = name;
+        this.position = position;
+        this.salary = salary;
+    }
 
     public Employee() {
 
     }
 
-    public Employee(String s) {
-    }
-
-    public int getId() {
-        return Id;
-    }
-
-    public void setId(int id) {
-        Id = id;
-    }
-
-    public String getName() {
-        return Name;
-    }
-
-    public void setName(String name) {
-        Name = name;
-    }
-
-    public String getPosition() {
-        return Position;
-    }
-
-    public void setPosition(String position) {
-        Position = position;
-    }
-
-    public String getSalary() {
-        return Salary;
-    }
-
-    public void setSalary(String salary) {
-        Salary = salary;
-    }
-
-    private String Salary;
-
-    public Employee(int id, String name, String Position, String Salary) {
-        this.Id = id;
-        this.Name = name;
-        this.Position = Position;
-        this.Salary = Salary;
-    }
-
-
     public String toString() {
-        return "Employee [id=" + Id + ", name=" + Name + ", dob=" + Salary + ", email=" + Position
-                + "]";
+        return " ID: " + id + "; NAME: " + name + "\n";
+    }
 
+    @Override
+    public int compareTo(Employee employee) {
+        int result = this.name.compareTo(employee.name);
+        if (result == 0)
+            return Integer.compare(this.id, employee.id);
+
+        return result;
     }
 }
+
+
 
